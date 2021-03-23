8b4658a7add3de3508f44f76704f3554a46b40e0
leekheang <ly.kheang.jr@gmail.com>
2021-03-23 09:59:44 +0700

    late update full space

---
+++ Activities/QuizDescriptionActivity.cs
@@ -0,0 +1,41 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.V7.App;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Quiz_App.Activities
+{
+    [Activity(Label = "QuizDescriptionActivity", Theme = "@style/AppTheme")]
+    public class QuizDescriptionActivity : AppCompatActivity
+
+        
+    {
+        TextView quizTopicTextView;
+        TextView descriptionTextView;
+        ImageView quizImageView;
+        Button startQuizButtion;
+        // Variable 
+        string quizTopic;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+            SetContentView(Resource.Layout.selected_topic);
+
+            quizTopicTextView = (TextView)FindViewById(Resource.Id.quizTopicText);
+            descriptionTextView = (TextView)FindViewById(Resource.Id.quizDescriptionText);
+            quizImageView = (ImageView)FindViewById(Resource.Id.quizeImage);
+            startQuizButtion = (Button)FindViewById(Resource.Id.quizStartQuizButton);
+            quizTopic = Intent.GetStringExtra("topic");
+            
+        }
+    }
+}
