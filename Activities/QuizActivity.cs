d59fc79e8cfbe168b06095d002c2b4a486ee39b5
leekheang <ly.kheang.jr@gmail.com>
2021-05-04 14:56:43 +0700

    optionRadio

--- Activities/QuizActivity.cs
+++ Activities/QuizActivity.cs
@@ -16,7 +16,10 @@ namespace Quiz_App.Activities
         
     public class QuizActivity : AppCompatActivity
     {
-        Android.Support.V7.Widget.Toolbar toolbar; 
+        Android.Support.V7.Widget.Toolbar toolbar;
+        RadioButton optionARadio, optionBRadio, optionCRadio, optionDRadio;
+        TextView optionATextView, optionBTextView, optionCTextView, optionDTextView, questionTextView, quizPositionTextView,timerConterTextView;
+        Button proccedQuizButtion;
         string quizTopic;
         protected override void OnCreate(Bundle savedInstanceState)
         {
@@ -30,7 +33,24 @@ namespace Quiz_App.Activities
             Android.Support.V7.App.ActionBar actionBar = SupportActionBar;
             actionBar.SetHomeAsUpIndicator(Resource.Drawable.outline_arrowback);
             actionBar.SetDisplayHomeAsUpEnabled(true);
+            ConnectViews();
             
+        }
+        void ConnectViews()
+        {
+            optionARadio = (RadioButton)FindViewById(Resource.Id.optionARadio);
+            optionBRadio = (RadioButton)FindViewById(Resource.Id.optionBRadio);
+            optionCRadio = (RadioButton)FindViewById(Resource.Id.optionCRadio);
+            optionDRadio = (RadioButton)FindViewById(Resource.Id.optionDRadio);
+
+            optionATextView = (TextView)FindViewById(Resource.Id.optionATextView);
+            optionBTextView = (TextView)FindViewById(Resource.Id.optionBTextView);
+            optionCTextView = (TextView)FindViewById(Resource.Id.optionCTextView);
+            optionDTextView = (TextView)FindViewById(Resource.Id.optionDTextView);
+
+            questionTextView = (TextView)FindViewById(Resource.Id.questionTextView);
+            quizPositionTextView = (TextView)FindViewById(Resource.Id.quizPositionTextView);
+            timerConterTextView = (TextView)FindViewById(Resource.Id.timeCounterTextView);
         }
     }
 }
